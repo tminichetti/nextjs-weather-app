901e7506bedc29f47b26285b83cb6f3586f0a9c7
Thomas Minichetti <thomas@trendex.tech>
2024-05-15 11:02:33 +0200

    updated all widgets

--- components/widgets/Visibility.tsx
+++ components/widgets/Visibility.tsx
@@ -4,7 +4,7 @@ import { Card } from "../ui/card"
 export const Visibility = ({ distance }: any) => {
     return (
         <Card className="w-full p-4 h-48 justify-between">
-            <span className="flex flex-row font-semibold text-sm text-neutral-600 dark:text-neutral-400 tracking-tight gap-2 mb-4"><Eye className="w-[16px]" />Visibility</span>
+            <span className="flex flex-row items-center font-semibold text-sm text-neutral-600 dark:text-neutral-400 tracking-tight gap-2 mb-4"><Eye className="w-[16px]" />Visibility</span>
             <span className="font-semibold md:text-lg">{distance} km</span>
         </Card>
     )
--- components/widgets/WeatherWidgets.tsx
+++ components/widgets/WeatherWidgets.tsx
@@ -3,12 +3,14 @@ import { Humidity } from "./Humidity"
 import { Pressure } from "./Pressure"
 import { UVIndex } from "./UVIndex"
 import { Visibility } from "./Visibility"
+import { Wind } from "./Wind"
 
 export const WeatherWidgets = ({ data }: any) => {
 
 
     return (
         <section className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-full gap-4">
+            <Wind speed={data.current.wind_kph} degree={data.current.wind_degree} />
             <UVIndex index={data.current.uv} />
             <FeelsLike temperature={data.current.feelslike_c} />
             <Humidity percent={data.current.humidity} />
